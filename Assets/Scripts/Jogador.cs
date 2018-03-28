66789e21b8044e3320eafe86ff8e8c7d1e860542
Paulo Salvatore <salvatoregames@gmail.com>
2018-03-28 07:31:13 -0300

    Initial commit

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+	public GameObject projetilPrefab;
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Atirar();
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			Debug.DrawLine(
+				Camera.main.transform.position,
+				hit.point
+			);
+
+			Debug.Log(hit.transform.name);
+		}
+	}
+
+	private void Atirar()
+	{
+		GameObject projetil = Instantiate(projetilPrefab);
+		projetil.transform.position = Camera.main.transform.position;
+		projetil.transform.forward = Camera.main.transform.forward;
+	}
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+	public float velocidade = 5f;
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+		transform.Translate(transform.forward * velocidade * Time.smoothDeltaTime);
+	}
+}
