30a4d8b8cde3904e67a48028b8e22c07dd6ee6d7
Paulo Salvatore <salvatoregames@gmail.com>
2018-04-03 11:59:26 -0300

    Gerador de Zumbis criado

--- Assets/Scripts/GeradorZumbis.cs
+++ Assets/Scripts/GeradorZumbis.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeradorZumbis : MonoBehaviour {
+public class GeradorZumbis : MonoBehaviour
+{
+    public float intervalo = 3f;
+    public float area = 4f;
+    public GameObject zumbiPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        InvokeRepeating("GerarZumbi", intervalo, intervalo);
+    }
+
+    private void GerarZumbi()
+    {
+        GameObject zumbi = Instantiate(zumbiPrefab);
+        Vector2 posicaoAleatoria = Random.insideUnitCircle * area;
+        zumbi.transform.position = new Vector3(
+            posicaoAleatoria.x,
+            0f,
+            posicaoAleatoria.y
+        );
+    }
 }
