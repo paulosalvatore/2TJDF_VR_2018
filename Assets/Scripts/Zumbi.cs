2b6b590dc7d8f6ebe316270068d7b5ae7a823a07
Paulo Salvatore <salvatoregames@gmail.com>
2018-04-03 12:04:32 -0300

    Movimentação do Zumbi adicionada

--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zumbi : MonoBehaviour {
+public class Zumbi : MonoBehaviour
+{
+	private bool andando = false;
+	public float delayAndar = 1.5f;
+
+	public Rigidbody rb;
+	public float velocidade = 0.35f;
+
+	private GameObject jogador;
 
 	// Use this for initialization
-	void Start () {
-		
+	private void Start()
+	{
+		jogador = GameObject.FindGameObjectWithTag("Player");
+
+		transform.LookAt(jogador.transform);
+
+		Invoke("Andar", delayAndar);
 	}
-	
+
+	private void Andar()
+	{
+		andando = true;
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update()
+	{
+		if (andando)
+		{
+			rb.velocity = (jogador.transform.position - transform.position).normalized * velocidade;
+		}
 	}
 }
